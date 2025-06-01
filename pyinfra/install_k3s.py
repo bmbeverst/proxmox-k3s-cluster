fe1a54dd4b7832ea254cce71f058a8047b480ee9
Brooks <603632+bmbeverst@users.noreply.github.com>
2025-05-31 22:36:51 -0400

    fix: Pyinfra works!

--- pyinfra/install_k3s.py
+++ pyinfra/install_k3s.py
@@ -1,13 +1,14 @@
-
+"""
+This script will install k3s on a host and configure journald to not use so much memory.
+It will first install on the init node,
+then checks that kuberentes is running and then installs on all other nodes.
+Skips the k3s install if already installed.
+"""
 
 from pyinfra import host
 from pyinfra.facts.files import Directory
-from pyinfra.operations import files, server, systemd, python
+from pyinfra.operations import files, python, server, systemd
 from pyinfra.operations.util import any_changed
-
-# Check if k3s is already installed
-if host.get_fact(Directory, "/etc/rancher/k3s/k3s.yaml"):
-    raise Exception("k3s is already installed")
 
 # Get TLS SAN from host info
 tls_san = host.data.get("tls_san")
@@ -26,69 +27,73 @@ def wait_for_init_node(init_node_ip):
         )
         if result.did_succeed():
             break
-        if attempt == 2:  # Last attempt
+        if attempt == 5:  # Last attempt
             print(f"Error: Could not connect to init node at {init_node_ip}:6443")
             print(f"Last error: {stderr}")
             raise Exception(f"Failed to connect to init node after 3 attempts")
-    
 
 
-if is_init_node:
-    # Install k3s on init node
-    server.shell(
-        name="Install k3s on init node",
-        commands=[
-            f'curl -sfL https://get.k3s.io | sh -s - --secrets-encryption --token "{k3s_token}" --tls-san "{tls_san}" --cluster-init'
-        ],
-    )
-    
-    # Wait for k3s to be ready on init node
-    server.shell(
-        name="Wait for k3s to be ready on init node",
-        commands=[
-            'until systemctl is-active k3s; do sleep 1; done'
-        ],
-    )
+# Check if k3s is already installed
+if host.get_fact(Directory, "/etc/rancher/k3s/"):
+    print("k3s is already installed")
 else:
-    # Get init node IP
-    init_node_ip = host.data.get("init_node_ip")
+    if is_init_node:
+        # Install k3s on init node
+        server.shell(
+            name="Install k3s on init node",
+            commands=[
+                f'curl -sfL https://get.k3s.io | sh -s - --secrets-encryption --token "{k3s_token}" --tls-san "{tls_san}" --cluster-init'  # pylint: disable=line-too-long
+            ],
+        )
 
-    python.call(
-        name="Wait for init node to respond",
-        function=wait_for_init_node,
-        init_node_ip=init_node_ip,
-    )
-    # Install k3s on worker node
-    server.shell(
-        name="Install k3s on worker node",
-        commands=[
-            f'curl -sfL https://get.k3s.io | sh -s - --secrets-encryption --token "{k3s_token}" --tls-san "{tls_san}" --server https://"{init_node_ip}":6443'
-        ],
-    )
+        # Wait for k3s to be ready on init node
+        # TODO fix that this executes after the curl commands, which is not correct
+        # server.shell(
+        #     name="Wait for k3s to be ready on init node",
+        #     commands=[
+        #         'until systemctl is-active k3s; do sleep 1; done'
+        #     ],
+        # )
+    else:
+        # Get init node IP
+        init_node_ip = host.data.get("init_node_ip")
+
+        python.call(
+            name="Wait for init node to respond",
+            function=wait_for_init_node,
+            init_node_ip=init_node_ip,
+        )
+        # Install k3s on worker node
+        server.shell(
+            name="Install k3s on worker node",
+            commands=[
+                f'curl -sfL https://get.k3s.io | sh -s - --secrets-encryption --token "{k3s_token}" --tls-san "{tls_san}" --server "https://{init_node_ip}:6443"'  # pylint: disable=line-too-long
+            ],
+            # _timeout=120, # This can be an issue due to GitHub throttling
+        )
 
 # Configure journald
-journald_changes = [
-    files.line(
-        name="Set SystemMaxUse in journald.conf",
-        path="/etc/systemd/journald.conf",
-        line="SystemMaxUse=256M    # Maximum total journal size",
-    ),
-    files.line(
-        name="Set RuntimeMaxUse in journald.conf",
-        path="/etc/systemd/journald.conf",
-        line="RuntimeMaxUse=128M   # Maximum journal size in temporary storage",
-    ),
-    files.line(
-        name="Set MaxFileSec in journald.conf",
-        path="/etc/systemd/journald.conf",
-        line="MaxFileSec=1month    # Maximum time to retain log files",
-    ),
-]
+sys_max = files.line(
+    name="Set SystemMaxUse in journald.conf",
+    path="/etc/systemd/journald.conf",
+    line="SystemMaxUse=256M    # Maximum total journal size",
+)
+run_max = files.line(
+    name="Set RuntimeMaxUse in journald.conf",
+    path="/etc/systemd/journald.conf",
+    line="RuntimeMaxUse=128M   # Maximum journal size in temporary storage",
+)
+file_max = files.line(
+    name="Set MaxFileSec in journald.conf",
+    path="/etc/systemd/journald.conf",
+    line="MaxFileSec=1month    # Maximum time to retain log files",
+)
+
 
 # Restart journald if any configuration was changed
 systemd.service(
     "systemd-journald",
     running=True,
     restarted=True,
-    _if=any_changed(journald_changes),
+    _if=any_changed(sys_max, run_max, file_max),
 )
--- pyinfra/uninstall_k3s.py
+++ pyinfra/uninstall_k3s.py
@@ -1,24 +1,15 @@
-from pyinfra import host
-from pyinfra.facts.files import Block
-from pyinfra.operations import python, server
+"""
+This script will uninstall k3s from the host.
+It checks if the k3s-uninstall.sh script exists and then executes it.
+"""
 
-# Check if k3s is installed by looking for the uninstall script
+from pyinfra import host
+
 K3S_UNINSTALL_SCRIPT = "/opt/bin/k3s-uninstall.sh"
 
-if not host.get_fact(Block, K3S_UNINSTALL_SCRIPT):
-    # Your code here...
+if host.get_fact(Block, K3S_UNINSTALL_SCRIPT):
     uninstall_result = server.shell(
-        name=f"Uninstall k3s on {host.name}",
-        commands=[f"{K3S_UNINSTALL_SCRIPT}"]
+        name=f"Uninstall k3s on {host.name}", commands=[f"{K3S_UNINSTALL_SCRIPT}"]
     )
-
-
-def callback():
-    print(f"Uninstall output for {host.name}:")
-    print(f"Got result: {uninstall_result.stdout}")
-
-
-python.call(
-    name="Execute callback function",
-    function=callback,
-)
+else:
+    print(f"k3s is already uninstalled on {host.name}")
