7b45df48b1341cbf7934415216757da15e22d424
Brooks <603632+bmbeverst@users.noreply.github.com>
2025-06-29 23:16:30 -0400

    fix: Use the right org

--- pulumi/infra-bootstrap/__main__.py
+++ pulumi/infra-bootstrap/__main__.py
@@ -1,16 +1,21 @@
 """A Kubernetes Python Pulumi program to create a Kubernetes namespace and secret with PKO"""
 
+from pulumi_kubernetes import core, rbac
 from pulumi_kubernetes.apiextensions import CustomResource
 from pulumi_kubernetes.core.v1 import Namespace, Secret
-from pulumi_kubernetes.helm.v3 import Chart, ChartOpts
+from pulumi_kubernetes.helm.v3 import Release, ReleaseArgs, RepositoryOptsArgs
 from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
 
 from pulumi import Config, ResourceOptions
 
-# Create a new Kubernetes Namespace
+# Create new Kubernetes Namespaces
 namespace = Namespace(
     "pulumi-kubernetes-operator",
     metadata=ObjectMetaArgs(name="pulumi-kubernetes-operator"),
+)
+namespace = Namespace(
+    "infra-apps",
+    metadata=ObjectMetaArgs(name="infra-apps"),
 )
 
 # Get the Pulumi API token.
@@ -30,18 +35,77 @@ pulumi_api_key = Secret(
     opts=ResourceOptions(depends_on=[namespace]),
 )
 
-pko = Chart(
+# Create a new Kubernetes Secret for the API Key
+gitlab_api_key = Secret(
+    "gitlab-access-token",
+    metadata={
+        "name": "gitlab-access-token",
+        "namespace": "pulumi-kubernetes-operator",
+    },
+    string_data={"access_token": gitlab_access_token},
+    opts=ResourceOptions(depends_on=[namespace]),
+)
+
+# Create ServiceAccount in the pulumi-kubernetes-operator namespace
+service_account = core.v1.ServiceAccount(
+    "pulumi-operator-sa",
+    metadata={"name": "pulumi", "namespace": "pulumi-kubernetes-operator"},
+)
+
+# Create required ClusterRoleBinding for system:auth-delegator
+auth_delegator_binding = rbac.v1.ClusterRoleBinding(
+    "pulumi-operator-auth-delegator",
+    metadata={"name": "pulumi-kubernetes-operator:pulumi:system:auth-delegator"},
+    role_ref={
+        "apiGroup": "rbac.authorization.k8s.io",
+        "kind": "ClusterRole",
+        "name": "system:auth-delegator",
+    },
+    subjects=[
+        {
+            "kind": "ServiceAccount",
+            "name": "pulumi",
+            "namespace": "pulumi-kubernetes-operator",
+        }
+    ],
+)
+
+# Add cluster-admin permissions if your Stack manages Kubernetes resources
+cluster_admin_binding = rbac.v1.ClusterRoleBinding(
+    "pulumi-operator-cluster-admin",
+    metadata={"name": "pulumi-kubernetes-operator:pulumi:cluster-admin"},
+    role_ref={
+        "apiGroup": "rbac.authorization.k8s.io",
+        "kind": "ClusterRole",
+        "name": "cluster-admin",
+    },
+    subjects=[
+        {
+            "kind": "ServiceAccount",
+            "name": "pulumi",
+            "namespace": "pulumi-kubernetes-operator",
+        }
+    ],
+)
+
+pko = Release(
     "pulumi-kubernetes-operator",
-    ChartOpts(
+    ReleaseArgs(
         chart="oci://ghcr.io/pulumi/helm-charts/pulumi-kubernetes-operator",
         namespace="pulumi-kubernetes-operator",
         version="2.0.0",
+        create_namespace=True,  # Optional: creates namespace if it doesn't exist
+        timeout=600,
+        atomic=True,  # Rollback on failure
     ),
-    opts=ResourceOptions(depends_on=[pulumi_api_key]),
 )
 
 infra_apps = CustomResource(
     "infra-apps",
+    metadata={
+        "name": "infra-apps",
+        "namespace": "pulumi-kubernetes-operator",
+    },
     api_version="pulumi.com/v1",
     kind="Stack",
     # There is no way to check these values in the pulumi code
@@ -56,43 +120,57 @@ infra_apps = CustomResource(
                 },
             },
         },
-        "stack": "infra-apps/dev",
+        "stack": "cyb3rkn19ht/infra-apps",
         "projectRepo": "https://gitlab.com/bmbeverst/proxmox-k3s-cluster.git",
         "repoDir": "pulumi/infra-apps/",
         "branch": "main",
-        "destroyOnFinalize": True,
-        "auth": {
-            "type": "personalAccessToken",
-            "personalAccessToken": gitlab_access_token,
-        },
-    },
-    opts=ResourceOptions(depends_on=[pko]),
-)
-
-infra_bootstrap = CustomResource(
-    "infra-bootstrap",
-    api_version="pulumi.com/v1",
-    kind="Stack",
-    spec={
-        "serviceAccountName": "pulumi",
-        "envRefs": {
-            "PULUMI_ACCESS_TOKEN": {
+        "gitAuth": {
+            "accessToken": {
                 "type": "Secret",
                 "secret": {
-                    "name": pulumi_api_key.metadata.name,
+                    "name": gitlab_api_key.metadata.name,
                     "key": "access_token",
                 },
             },
         },
-        "stack": "infra-bootstrap/dev",
-        "projectRepo": "https://gitlab.com/bmbeverst/proxmox-k3s-cluster.git",
-        "repoDir": "pulumi/infra-bootstrap/",
-        "branch": "main",
         "destroyOnFinalize": True,
-        "auth": {
-            "type": "personalAccessToken",
-            "personalAccessToken": gitlab_access_token,
-        },
     },
     opts=ResourceOptions(depends_on=[pko]),
 )
+
+# infra_bootstrap = CustomResource(
+#     "infra-bootstrap",
+#     metadata={
+#         "name": "infra-bootstrap",
+#         "namespace": "pulumi-kubernetes-operator",
+#     },
+#     api_version="pulumi.com/v1",
+#     kind="Stack",
+#     spec={
+#         "serviceAccountName": "pulumi",
+#         "envRefs": {
+#             "PULUMI_ACCESS_TOKEN": {
+#                 "type": "Secret",
+#                 "secret": {
+#                     "name": pulumi_api_key.metadata.name,
+#                     "key": "access_token",
+#                 },
+#             },
+#         },
+#         "stack": "infra-bootstrap/dev",
+#         "projectRepo": "https://gitlab.com/bmbeverst/proxmox-k3s-cluster.git",
+#         "repoDir": "pulumi/infra-bootstrap/",
+#         "branch": "main",
+#         "gitAuth": {
+#             "accessToken": {
+#                 "type": "Secret",
+#                 "secret": {
+#                     "name": gitlab_api_key.metadata.name,
+#                     "key": "access_token",
+#                 },
+#             },
+#         },
+#         "destroyOnFinalize": True,
+#     },
+#     opts=ResourceOptions(depends_on=[pko]),
+# )
