80d359fe803b69ab0d36c9302738273c776b7f8e
Brooks <603632+bmbeverst@users.noreply.github.com>
2026-05-22 14:35:06 -0400

    feat: Add plan for system-upgrade and commit the right file

--- pulumi/infra-apps/__main__.py
+++ pulumi/infra-apps/__main__.py
@@ -1,6 +1,9 @@
+"""These are the apps that are setup to run the cluster infrastructure"""
+
+from pulumi_kubernetes.apiextensions import CustomResource
 from pulumi_kubernetes.yaml.v2 import ConfigFile
 
-import pulumi
+from pulumi import ResourceOptions
 
 SUC_VERSION = "v0.19.2"
 
@@ -12,5 +15,66 @@ suc_crd = ConfigFile(
 suc = ConfigFile(
     "system-upgrade-controller",
     file=f"https://github.com/rancher/system-upgrade-controller/releases/download/{SUC_VERSION}/system-upgrade-controller.yaml",
-    opts=pulumi.ResourceOptions(depends_on=[suc_crd]),
+    opts=ResourceOptions(depends_on=[suc_crd]),
+)
+
+server_plan = CustomResource(
+    "server-plan",
+    api_version="upgrade.cattle.io/v1",
+    kind="Plan",
+    metadata={
+        "name": "server-plan",
+        "namespace": "system-upgrade",
+    },
+    spec={
+        "concurrency": 1,
+        "cordon": True,
+        "nodeSelector": {
+            "matchExpressions": [
+                {
+                    "key": "node-role.kubernetes.io/control-plane",
+                    "operator": "In",
+                    "values": ["true"],
+                }
+            ],
+        },
+        "serviceAccountName": "system-upgrade",
+        "upgrade": {
+            "image": "rancher/k3s-upgrade",
+        },
+        "channel": "https://update.k3s.io/v1-release/channels/stable",
+    },
+    opts=ResourceOptions(depends_on=[suc]),
+)
+
+agent_plan = CustomResource(
+    "agent-plan",
+    api_version="upgrade.cattle.io/v1",
+    kind="Plan",
+    metadata={
+        "name": "agent-plan",
+        "namespace": "system-upgrade",
+    },
+    spec={
+        "concurrency": 1,
+        "cordon": True,
+        "nodeSelector": {
+            "matchExpressions": [
+                {
+                    "key": "node-role.kubernetes.io/control-plane",
+                    "operator": "DoesNotExist",
+                }
+            ],
+        },
+        "serviceAccountName": "system-upgrade",
+        "prepare": {
+            "image": "rancher/k3s-upgrade",
+            "args": ["prepare", "server-plan"],
+        },
+        "upgrade": {
+            "image": "rancher/k3s-upgrade",
+        },
+        "channel": "https://update.k3s.io/v1-release/channels/stable",
+    },
+    opts=ResourceOptions(depends_on=[server_plan]),
 )
