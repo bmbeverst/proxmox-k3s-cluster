16eb462603ec2d79bdf54b96ad5bd2572b019884
Brooks <603632+bmbeverst@users.noreply.github.com>
2026-05-22 20:00:23 -0400

    feat: Lets listen to the crazy AI and see if it works.

--- pulumi/infra-apps/__main__.py
+++ pulumi/infra-apps/__main__.py
@@ -1,21 +1,77 @@
 """These are the apps that are setup to run the cluster infrastructure"""
 
+from typing import Optional
+import pulumi
+import pulumi_kubernetes as k8s
+
 from pulumi_kubernetes.apiextensions import CustomResource
 from pulumi_kubernetes.yaml.v2 import ConfigFile
 
 from pulumi import ResourceOptions
 
+
 SUC_VERSION = "v0.19.2"
 
-suc_crd = ConfigFile(
+
+def patch_suc_deployment(
+    args: pulumi.ResourceTransformationArgs,
+) -> Optional[pulumi.ResourceTransformationResult]:
+    # Only touch the Deployment, leave other resources in the manifest alone
+    if args.type_ != "kubernetes:apps/v1:Deployment":
+        return None
+
+    props = args.props
+    template_spec = props["spec"]["template"]["spec"]
+
+    # Add volume mounts to all containers (there's only one in the SUC deployment)
+    for container in template_spec.get("containers", []):
+        container.setdefault("volumeMounts", []).extend(
+            [
+                {
+                    "mountPath": "/usr/share/ssl",
+                    "name": "usr-share-ssl",
+                    "readOnly": True,
+                },
+                {
+                    "mountPath": "/usr/share/ca-certificates",
+                    "name": "usr-share-ca-certificates",
+                    "readOnly": True,
+                },
+            ]
+        )
+
+    # Add the corresponding host path volumes
+    template_spec.setdefault("volumes", []).extend(
+        [
+            {
+                "hostPath": {"path": "/usr/share/ssl", "type": "DirectoryOrCreate"},
+                "name": "usr-share-ssl",
+            },
+            {
+                "hostPath": {
+                    "path": "/usr/share/ca-certificates",
+                    "type": "DirectoryOrCreate",
+                },
+                "name": "usr-share-ca-certificates",
+            },
+        ]
+    )
+
+    return pulumi.ResourceTransformationResult(props=props, opts=args.opts)
+
+
+suc_crd = k8s.yaml.v2.ConfigFile(
     "system-upgrade-controller-crd",
     file=f"https://github.com/rancher/system-upgrade-controller/releases/download/{SUC_VERSION}/crd.yaml",
 )
 
-suc = ConfigFile(
+suc = k8s.yaml.v2.ConfigFile(
     "system-upgrade-controller",
     file=f"https://github.com/rancher/system-upgrade-controller/releases/download/{SUC_VERSION}/system-upgrade-controller.yaml",
-    opts=ResourceOptions(depends_on=[suc_crd]),
+    opts=pulumi.ResourceOptions(
+        depends_on=[suc_crd],
+        transformations=[patch_suc_deployment],
+    ),
 )
 
 server_plan = CustomResource(
