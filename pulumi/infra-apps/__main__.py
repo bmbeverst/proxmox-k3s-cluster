56e848fb8d3b814b26f0371d168cecfeab7d2796
Brooks <603632+bmbeverst@users.noreply.github.com>
2026-07-04 14:04:11 -0400

    fix: Fix the SSL config

--- pulumi/infra-apps/__main__.py
+++ pulumi/infra-apps/__main__.py
@@ -13,52 +13,6 @@ from pulumi import ResourceOptions
 SUC_VERSION = "v0.19.2"
 
 
-def patch_suc_deployment(
-    args: pulumi.ResourceTransformationArgs,
-) -> Optional[pulumi.ResourceTransformationResult]:
-    # Only touch the Deployment, leave other resources in the manifest alone
-    if args.type_ != "kubernetes:apps/v1:Deployment":
-        return None
-
-    props = args.props
-    template_spec = props["spec"]["template"]["spec"]
-
-    # Add volume mounts to all containers (there's only one in the SUC deployment)
-    for container in template_spec.get("containers", []):
-        container.setdefault("volumeMounts", []).extend(
-            [
-                {
-                    "mountPath": "/usr/share/ssl",
-                    "name": "usr-share-ssl",
-                    "readOnly": True,
-                },
-                {
-                    "mountPath": "/usr/share/ca-certificates",
-                    "name": "usr-share-ca-certificates",
-                    "readOnly": True,
-                },
-            ]
-        )
-
-    # Add the corresponding host path volumes
-    template_spec.setdefault("volumes", []).extend(
-        [
-            {
-                "hostPath": {"path": "/usr/share/ssl", "type": "DirectoryOrCreate"},
-                "name": "usr-share-ssl",
-            },
-            {
-                "hostPath": {
-                    "path": "/usr/share/ca-certificates",
-                    "type": "DirectoryOrCreate",
-                },
-                "name": "usr-share-ca-certificates",
-            },
-        ]
-    )
-
-    return pulumi.ResourceTransformationResult(props=props, opts=args.opts)
-
 
 suc_crd = k8s.yaml.v2.ConfigFile(
     "system-upgrade-controller-crd",
@@ -70,7 +24,6 @@ suc = k8s.yaml.v2.ConfigFile(
     file=f"https://github.com/rancher/system-upgrade-controller/releases/download/{SUC_VERSION}/system-upgrade-controller.yaml",
     opts=pulumi.ResourceOptions(
         depends_on=[suc_crd],
-        transformations=[patch_suc_deployment],
     ),
 )
 
@@ -133,4 +86,53 @@ agent_plan = CustomResource(
         "channel": "https://update.k3s.io/v1-release/channels/stable",
     },
     opts=ResourceOptions(depends_on=[server_plan]),
+)
+
+
+suc_patch = k8s.apps.v1.DeploymentPatch(
+    "system-upgrade-controller-patch",
+    metadata=k8s.meta.v1.ObjectMetaPatchArgs(
+        name="system-upgrade-controller",
+        namespace="system-upgrade",
+    ),
+    spec=k8s.apps.v1.DeploymentSpecPatchArgs(
+        template=k8s.core.v1.PodTemplateSpecPatchArgs(
+            spec=k8s.core.v1.PodSpecPatchArgs(
+                containers=[
+                    k8s.core.v1.ContainerPatchArgs(
+                        name="system-upgrade-controller",
+                        volume_mounts=[
+                            k8s.core.v1.VolumeMountPatchArgs(
+                                mount_path="/usr/share/ssl",
+                                name="usr-share-ssl",
+                                read_only=True,
+                            ),
+                            k8s.core.v1.VolumeMountPatchArgs(
+                                mount_path="/usr/share/ca-certificates",
+                                name="usr-share-ca-certificates",
+                                read_only=True,
+                            ),
+                        ],
+                    )
+                ],
+                volumes=[
+                    k8s.core.v1.VolumePatchArgs(
+                        name="usr-share-ssl",
+                        host_path=k8s.core.v1.HostPathVolumeSourcePatchArgs(
+                            path="/usr/share/ssl",
+                            type="DirectoryOrCreate",
+                        ),
+                    ),
+                    k8s.core.v1.VolumePatchArgs(
+                        name="usr-share-ca-certificates",
+                        host_path=k8s.core.v1.HostPathVolumeSourcePatchArgs(
+                            path="/usr/share/ca-certificates",
+                            type="DirectoryOrCreate",
+                        ),
+                    ),
+                ],
+            )
+        )
+    ),
+    opts=pulumi.ResourceOptions(depends_on=[suc]),
 )
